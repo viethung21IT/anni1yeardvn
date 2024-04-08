6a1fd5de54aa9b42b78ea6263763e9ab465c9271
viethung21IT <viethungboy2005@gmail.com>
2024-04-08 21:05:50 +0700

    super up

---
+++ script.js
@@ -0,0 +1,29 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+const questionPrompt = document.querySelector(".question-prompt"); // Thêm đoạn này
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Trong đây hông có gì đâu chỉ có Đan Vy thoai<br>coi xong ròi load lại trang bấm nút kế bên nhé🤩";
+  gif.src =
+    "https://media.giphy.com/media/vvc1dJPLEU2QOw9cXy/giphy.gif";
+
+  // Ẩn cả hai nút Yes và No
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
+  questionPrompt.style.display = "none"; // Ẩn dòng chữ khi người dùng nhấn vào nút Yes
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
